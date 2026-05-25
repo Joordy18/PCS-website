b89d631a168bb02a5ebcb554564189e5e0c909ed
Lucas Arcizet <164637154+Joordy18@users.noreply.github.com>
2026-05-25 19:55:51 +0200

    test

---
+++ public/js/main.04286dbc41eea958aace078698768d354eb3f6a8f2f042b6c69ab08ca86dd9f1.js
@@ -0,0 +1,1 @@
+(()=>{document.addEventListener("DOMContentLoaded",()=>{typeof lucide<"u"&&lucide.createIcons(),typeof AOS<"u"&&AOS.init({once:!0,duration:800,easing:"ease-out-cubic",offset:50})});})();
---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+(() => {
+  // <stdin>
+  document.addEventListener("DOMContentLoaded", () => {
+    if (typeof lucide !== "undefined") {
+      lucide.createIcons();
+    }
+    if (typeof AOS !== "undefined") {
+      AOS.init({
+        once: true,
+        duration: 800,
+        easing: "ease-out-cubic",
+        offset: 50
+      });
+    }
+  });
+})();
